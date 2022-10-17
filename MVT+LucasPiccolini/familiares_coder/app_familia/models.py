306a284c29bc3f3b22333ec8d4117911fcd8cfa3
Lucas Piccolini <lucas.piccolini@gmail.com>
2022-10-17 20:51:32 -0300

    Se migraron las tablas de models.py

--- MVT+LucasPiccolini/familiares_coder/app_familia/models.py
+++ MVT+LucasPiccolini/familiares_coder/app_familia/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Familiar(models.Model):
     edad = models.IntegerField()
-    nombre = models.CharField()
+    nombre = models.CharField(max_length=30)
     nacimiento = models.DateField()
 
